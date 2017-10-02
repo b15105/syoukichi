15ddc40766bcefcdcd89d88dcde350550b0b5ea6
0yukkey0 <eternityavis@gmail.com>
2017-10-02 17:48:33 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+//オートローダの指定
+require_once __DIR__ . '/vendor/autoload.php';
+
+//CurlHTTPClientとLINEBotのインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+//署名の検証作業
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
+
+//メッセージ型のチェックとオウム返し
+foreach ($events as $event) {
+	if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+	  error_log('Non message event has come');
+	  continue;
+	}
+	if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+	  error_log('Non text message has come');
+	  continue;
+	}
+
+	$text = $event->getText();
+	if($text == "画像"){
+		replyImageMessage($bot, $event->getReplyToken(), "https://" . $_SERVER["HTTP_HOST"] . "/imgs/original.jpg", "https://" . $_SERVER["HTTP_HOST"] . "/imgs/preview.jpg");
+	}
+}
+
+function replyTextMessage($bot, $replyToken, $text) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
+function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl) {
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
+ ?>
