c3d43b905c3396565d4da8585f26b0b68ead010c
b15105 <e1b15105@st.oit.ac.jp>
2017-10-23 18:53:36 +0900

    GCV composer 追加

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit8cd0c8b0769ad975e7929ab8a5eaade0
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8cd0c8b0769ad975e7929ab8a5eaade0::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8cd0c8b0769ad975e7929ab8a5eaade0($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8cd0c8b0769ad975e7929ab8a5eaade0($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
